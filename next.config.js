dbf3c49214dad1f45a068318b9460e7c35c41034
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-07-28 17:36:30 -0500

    Adding build steps (#5)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    // Generate a static export for GitHub Pages
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
